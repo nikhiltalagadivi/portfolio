ceec97a5a7022d88ca7c5a242f07ba24d24f794c
nikhiltalagadivi <133486834+nikhiltalagadivi@users.noreply.github.com>
2025-01-16 15:33:29 +1100

    Update java.js

--- java.js
+++ java.js
@@ -12,3 +12,25 @@ const handleOnMouseMove = e => {
 for(const card of document.querySelectorAll(".card")){
     card.onmousemove = e => handleOnMouseMove(e);
 }
+
+// Enable horizontal scrolling with mouse wheel and touchpad
+const wrapper = document.querySelector('.wrapper');
+let isTouchPad = false;
+
+wrapper.addEventListener('wheel', (event) => {
+    if (event.deltaY !== 0) {
+        if (!isTouchPad) {
+            event.preventDefault();
+            wrapper.scrollLeft += event.deltaY;
+        }
+    }
+});
+
+// Detect if the user is using a touchpad
+window.addEventListener('mousemove', (event) => {
+    if (event.wheelDeltaY) {
+        isTouchPad = false;
+    } else if (event.deltaMode === 0) {
+        isTouchPad = true;
+    }
+});
